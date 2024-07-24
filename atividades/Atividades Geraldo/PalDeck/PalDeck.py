9c594a827c070da389319eb9c99d6a98b96adae9
Geraldo Braian <geraldo.bryan.vbnb@gmail.com>
2024-07-23 21:46:47 -0300

    Adicionei o incrimento no valor de pals

--- atividades/Atividades Geraldo/PalDeck/PalDeck.py
+++ atividades/Atividades Geraldo/PalDeck/PalDeck.py
@@ -1,5 +1,11 @@
 nome = input ("Digite seu nome: ")
 print (f"Pal dex de {nome}")
 
-quantidade_pal = input ("Quantos pals você tem: ")
+quantidade_pal = int (input ("Quantos pals você tem: "))
 print (f"{nome} possuiu {quantidade_pal} pals")
+
+inserir = int (input ("Quantos pals novos você capturou: "))
+
+nova_quantidade = quantidade_pal + inserir
+
+print (f"{nome} capturou {inserir}! Agora tem {nova_quantidade}")
